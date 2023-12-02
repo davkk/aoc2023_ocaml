0f10f3e1155e5022fbf8e0601fd21ac5c629d213
davkk <dadulerk@gmail.com>
2023-12-02 14:45:56 +0100

    fix(day02 part1): use for_all instead of find/fold

--- bin/day02.ml
+++ bin/day02.ml
@@ -5,20 +5,15 @@ let lines = Advent.read_lines "./inputs/day02.in"
 let is_valid_game cubes =
   cubes
   |> Str.split (Str.regexp "; ")
-  |> List.fold ~init:true ~f:(fun acc player ->
-    let invalid =
-      player
-      |> Str.split (Str.regexp ", ")
-      |> List.find ~f:(fun color ->
-        match String.split ~on:' ' color with
-        | [ count; "red" ] when Int.of_string count > 12 -> true
-        | [ count; "green" ] when Int.of_string count > 13 -> true
-        | [ count; "blue" ] when Int.of_string count > 14 -> true
-        | _ -> false)
-    in
-    match invalid with
-    | Some _ -> false
-    | None -> if not acc then false else true)
+  |> List.for_all ~f:(fun player ->
+    player
+    |> Str.split (Str.regexp ", ")
+    |> List.for_all ~f:(fun color ->
+      match String.split ~on:' ' color with
+      | [ count; "red" ] when Int.of_string count > 12 -> false
+      | [ count; "green" ] when Int.of_string count > 13 -> false
+      | [ count; "blue" ] when Int.of_string count > 14 -> false
+      | _ -> true))
 ;;
 
 let () =
