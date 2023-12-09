154e1a32341d2fa41df6379344326086e482a090
davkk <dadulerk@gmail.com>
2023-12-09 23:57:15 +0100

    refactor(day07): abstract away some common logic

--- bin/day07.ml
+++ bin/day07.ml
@@ -1,11 +1,3 @@
-let lines = Advent.read_lines "./inputs/day07.in"
-
-type hand =
-  { cards : string
-  ; bid : int
-  }
-[@@deriving show]
-
 type hand_class =
   | High
   | OnePair
@@ -14,36 +6,56 @@ type hand_class =
   | FullHouse
   | Four
   | Five
-[@@deriving enum]
+[@@deriving show, enum]
+
+type hand =
+  { cards : int list
+  ; bid : int
+  ; class_ : hand_class
+  }
+[@@deriving show]
 
 module CardMap = Map.Make (Char)
 
-let process_hand line =
-  line
-  |> String.split_on_char ' '
-  |> function
-  | [ cards; bid ] -> { cards; bid = int_of_string bid }
-  | _ -> failwith "Invalid line"
+let process_hands lines ~card_to_number ~hand_to_class =
+  lines
+  |> List.map (fun line ->
+    line
+    |> String.split_on_char ' '
+    |> function
+    | [ cards; bid ] ->
+      { cards = String.to_seq cards |> Seq.map card_to_number |> List.of_seq
+      ; bid = int_of_string bid
+      ; class_ = hand_to_class cards
+      }
+    | _ -> failwith "Invalid line")
+  |> List.sort (fun hand_a hand_b ->
+    match hand_a.class_, hand_b.class_ with
+    | a, b when a = b -> compare hand_a.cards hand_b.cards
+    | _ ->
+      compare
+        (hand_class_to_enum hand_a.class_)
+        (hand_class_to_enum hand_b.class_))
 ;;
 
-let count_cards hand =
-  let counts_map = CardMap.empty in
-  let counts =
-    hand.cards
-    |> String.to_seq
-    |> Seq.fold_left
-         (fun map card ->
-           CardMap.update
-             card
-             (function
-               | Some count -> Some (count + 1)
-               | None -> Some 1)
-             map)
-         counts_map
-    |> CardMap.to_list
-    |> List.sort (fun a b -> compare (snd b) (snd a))
-  in
-  counts
+let count_cards cards =
+  cards
+  |> String.to_seq
+  |> Seq.fold_left
+       (fun map card ->
+         CardMap.update
+           card
+           (function
+             | Some count -> Some (count + 1)
+             | None -> Some 1)
+           map)
+       CardMap.empty
+  |> CardMap.to_list
+  |> List.sort (fun a b -> compare (snd b) (snd a))
+;;
+
+let total_winnings =
+  Base.List.foldi ~f:(fun idx acc hand -> acc + (hand.bid * (idx + 1))) ~init:0
 ;;
 
 module Part1 = struct
@@ -53,19 +65,12 @@ module Part1 = struct
     | 'Q' -> 12
     | 'J' -> 11
     | 'T' -> 10
-    | '9' -> 9
-    | '8' -> 8
-    | '7' -> 7
-    | '6' -> 6
-    | '5' -> 5
-    | '4' -> 4
-    | '3' -> 3
-    | '2' -> 2
+    | c when c <= '9' && c >= '2' -> Char.code c - Char.code '0'
     | _ -> 1
   ;;
 
-  let hand_to_class hand =
-    hand
+  let hand_to_class cards =
+    cards
     |> count_cards
     |> begin
          function
@@ -78,37 +83,16 @@ module Part1 = struct
          | [ _; _; _; _; _ ] -> High
          | _ -> failwith "invalid hand"
        end
-    |> hand_class_to_enum
   ;;
 end
 
 let () =
-  let hands =
-    lines
-    |> List.map process_hand
-    |> List.sort (fun hand_a hand_b ->
-      let class_a = Part1.hand_to_class hand_a in
-      let class_b = Part1.hand_to_class hand_b in
-      match class_a, class_b with
-      | a, b when a = b ->
-        let cards_a =
-          String.to_seq hand_a.cards
-          |> Seq.map Part1.card_to_number
-          |> List.of_seq
-        in
-        let cards_b =
-          String.to_seq hand_b.cards
-          |> Seq.map Part1.card_to_number
-          |> List.of_seq
-        in
-        compare cards_a cards_b
-      | _ -> compare class_a class_b)
-  in
   let result =
-    hands
-    |> Base.List.foldi
-         ~f:(fun idx acc hand -> acc + (hand.bid * (idx + 1)))
-         ~init:0
+    Advent.read_lines "./inputs/day07.in"
+    |> process_hands
+         ~card_to_number:Part1.card_to_number
+         ~hand_to_class:Part1.hand_to_class
+    |> total_winnings
   in
   Fmt.pr "@.%d@." result
 ;;
@@ -119,20 +103,13 @@ module Part2 = struct
     | 'K' -> 12
     | 'Q' -> 11
     | 'T' -> 10
-    | '9' -> 9
-    | '8' -> 8
-    | '7' -> 7
-    | '6' -> 6
-    | '5' -> 5
-    | '4' -> 4
-    | '3' -> 3
-    | '2' -> 2
+    | c when c <= '9' && c >= '2' -> Char.code c - Char.code '0'
     | 'J' -> 1
     | _ -> 0
   ;;
 
-  let hand_to_class hand =
-    hand
+  let hand_to_class cards =
+    cards
     |> count_cards
     |> begin
          function
@@ -161,37 +138,16 @@ module Part2 = struct
          | [ _; _; _; _; _ ] -> High
          | _ -> failwith "unknown hand"
        end
-    |> hand_class_to_enum
   ;;
 end
 
 let () =
-  let hands =
-    lines
-    |> List.map process_hand
-    |> List.sort (fun hand_a hand_b ->
-      let class_a = Part2.hand_to_class hand_a in
-      let class_b = Part2.hand_to_class hand_b in
-      match class_a, class_b with
-      | a, b when a = b ->
-        let cards_a =
-          String.to_seq hand_a.cards
-          |> Seq.map Part2.card_to_number
-          |> List.of_seq
-        in
-        let cards_b =
-          String.to_seq hand_b.cards
-          |> Seq.map Part2.card_to_number
-          |> List.of_seq
-        in
-        compare cards_a cards_b
-      | _ -> compare class_a class_b)
-  in
   let result =
-    hands
-    |> Base.List.foldi
-         ~f:(fun idx acc hand -> acc + (hand.bid * (idx + 1)))
-         ~init:0
+    Advent.read_lines "./inputs/day07.in"
+    |> process_hands
+         ~card_to_number:Part2.card_to_number
+         ~hand_to_class:Part2.hand_to_class
+    |> total_winnings
   in
   Fmt.pr "@.%d@." result
 ;;
